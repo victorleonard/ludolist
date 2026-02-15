3e31eebfb8d1c46a7f2b237207924b0ae37dd9e2
echo-vic <victorleonard@hotmail.fr>
2026-02-15 12:47:54 +0100

    fix(viewport): update viewport meta tag in nuxt.config.ts and app.vue to prevent zooming on mobile devices; add touch-action property in main.css to enhance PWA usability

--- @front/nuxt.config.ts
+++ @front/nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       meta: [
-        { name: 'viewport', content: 'width=device-width,initial-scale=1,viewport-fit=cover' },
+        { name: 'viewport', content: 'width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no,viewport-fit=cover' },
         { name: 'apple-mobile-web-app-capable', content: 'yes' },
         { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
         { name: 'theme-color', content: '#f9fafb' }
